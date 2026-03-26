4b823990bade95a9318aea98fa1f83b8378a5cb0
vladtoder <vladtoder3@gmail.com>
2026-03-26 18:41:41 +0000

    merged frontend and backend

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,9 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Missing Supabase environment variables');
+}
+
 const supabase = createClient(supabaseUrl, supabaseKey);
-
-console.log(supabaseUrl, supabaseKey);
-
 
 export default supabase;
